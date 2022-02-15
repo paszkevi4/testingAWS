742497cfc57034fd566136276e742085141df6c0
pkiias <pkiias@hssoft.com>
2022-02-15 12:23:59 +0200

    updated title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          AWS TESTING !!!
         </a>
       </header>
     </div>
